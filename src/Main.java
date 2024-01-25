f1b346e26998d01c38e609354fc41fb1972caacd
00DerTechniker00 <piet.wiesmann@gmail.com>
2024-01-25 17:55:58 +0100

    Controlling LEDs with a Rasberry Pi

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import pinlistener;
+
+public class Main implements PinListener, AutoCloseable
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+    }
+}
